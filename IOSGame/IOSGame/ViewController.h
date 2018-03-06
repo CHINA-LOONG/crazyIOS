c6e31806d1fed9b0df1690b07e5669bc84ecb474
china_loong <xuelong9009@qq.com>
2018-03-06 15:09:54 +0800

    13_chapter part01
    创建连连看项目，设置基础界面

---
+++ IOSGame/IOSGame/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  IOSGame
+//
+//  Created by 薛龙 on 2018/3/6.
+//  Copyright © 2018年 xuelong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIButton *startBtn;
+@property (strong, nonatomic) IBOutlet UILabel *timeText;
+
+@end
+
---
+++ IOSGame/IOSGame/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  IOSGame
+//
+//  Created by 薛龙 on 2018/3/6.
+//  Copyright © 2018年 xuelong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
